24ceab4508a4e2819bd7d8ae1c9dc02f1833e3f9
Gaurav <gaurav.naithani139@gmail.com>
2017-04-01 22:42:28 +0530

    test commit

---
+++ postgresql.rb
@@ -0,0 +1,15 @@
+package 'postgresql-server' do
+	notifies :run, 'execute[postgresql-init]'
+end
+
+execute 'postgresql-init' do
+	command 'postgresql-setup initdb'
+	action :nothing
+end
+
+service 'postgresql' do
+	action [:enable, :start]
+end
+
+
+
